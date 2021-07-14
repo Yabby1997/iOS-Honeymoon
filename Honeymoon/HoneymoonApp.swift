dc6bd4473d7049cf19517446fe4ac98e10c55651
yabby <yabby1997@gmail.com>
2021-07-14 13:21:19 +0900

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Seunghun Yang on 2021/07/14.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
